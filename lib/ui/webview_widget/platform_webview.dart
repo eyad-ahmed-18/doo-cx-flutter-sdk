65b8702060d398dda456f4abe7f9768180dee0f0
Eyad Ahmad <eyad@likwid.co>
2025-10-22 12:40:41 +0300

    merge changes

--- lib/ui/webview_widget/platform_webview.dart
+++ lib/ui/webview_widget/platform_webview.dart
@@ -2,3 +2,4 @@
 // On Web => use the real web implementation (uses dart:html / ui_web).
 // Else (Android/iOS/desktop) => use a harmless stub that doesn't import web libs.
 export 'web_webview_stud.dart' if (dart.library.html) 'web_webview.dart';
+
